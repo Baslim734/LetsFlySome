ece278ad6e6c32b1394b7115896f2d81799b7690
Baslim734 <nikita7343072@gmail.com>
2023-04-20 10:49:46 +0300

    Done animation start screen

---
+++ AuthViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AuthViewController.swift
+//  LetsFlySome
+//
+//  Created by Никита Бирюков on 20.04.2023.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .black
+    }
+    
+
+    
+
+}
